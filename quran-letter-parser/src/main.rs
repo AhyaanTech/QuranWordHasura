1f305fd4cb8879c0a75984be548d40f7c28a9683
AhyaanTech <ahyaantech@gmail.com>
2022-10-19 00:46:42 -0500

    figured out how to get tajweed data. now need to parse it out

--- quran-letter-parser/src/main.rs
+++ quran-letter-parser/src/main.rs
@@ -1,12 +1,20 @@
 use std::fs;
 
 use serde_json::{Result, Value};
+use unicode_segmentation::UnicodeSegmentation;
 
 fn main() -> Result<()> {
     let contents =
         fs::read_to_string("first.json").expect("Should have been able to read the file");
     let v: Value = serde_json::from_str(&contents)?;
-    let ayahs = v["ayahs"][0]["text"].to_owned().to_string().chars();
-    println!("With text:\n{:?}", ayahs);
+    let binding = &v["ayahs"][0]["text"].to_string();
+    let ayahs = binding
+        .unicode_words()
+        // .map(|f| f.escape_unicode().to_string())
+        .collect::<Vec<&str>>()
+        .into_iter()
+        .filter(|x| x.to_owned().ne("\"") && x.to_owned().ne("\u{feff}"))
+        .collect::<Vec<&str>>();
+    println!("With text:\n{:#?}", ayahs);
     Ok(())
 }
